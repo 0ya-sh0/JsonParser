0e99a122d6ccc6f2928d99dcaff6fc3480f8fccb
0ya-sh0 <ymthatte.gmail.com>
2022-06-05 21:54:12 +0530

    fix:error handling in demo.c

--- demo.c
+++ demo.c
@@ -10,11 +10,24 @@ int main(int argc, char const *argv[])
     const char *fname = argv[1];
 
     FILE *f = fopen(fname, "rb");
+
+    if (!f)
+    {
+        perror("fopen:");
+        exit(0);
+    }
+
     fseek(f, 0, SEEK_END);
     long fsize = ftell(f);
-    fseek(f, 0, SEEK_SET); /* same as rewind(f); */
+    fseek(f, 0, SEEK_SET);
 
     char *string = malloc(fsize + 1);
+
+    if (!string)
+    {
+        perror("malloc:");
+        exit(0);
+    }
     fread(string, fsize, 1, f);
     fclose(f);
 
